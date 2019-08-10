24835e1fe61ad4fb299de733f6004d52d3e2af7e
SongoMen <songomengaming@gmail.com>
2019-08-10 22:59:58 +0200

    changed urls

--- src/components/dashboard/panel.js
+++ src/components/dashboard/panel.js
@@ -79,10 +79,10 @@ class Panel extends React.Component {
   reLogin() {
     if (this.props.type === "instagram")
       window.location.href =
-        "https://api.instagram.com/oauth/authorize/?client_id=c3ef63a1c9de41a1b6032a7c39e586ae&redirect_uri=http://localhost:3000/addaccountinstagram&response_type=token&instagram&scope=public_content";
+        "https://api.instagram.com/oauth/authorize/?client_id=c3ef63a1c9de41a1b6032a7c39e586ae&redirect_uri=https://socialanalytics-94264.web.app/addaccountinstagram&response_type=token&instagram&scope=public_content";
     else if (this.props.type === "facebook")
       window.location.href =
-        "https://www.facebook.com/v4.0/dialog/oauth?response_type=token&client_id=2072731952831318&redirect_uri=http://localhost:3000/addaccountfacebook&auth_type=rerequest&scope=public_profile%2Cmanage_pages%2Cpages_messaging%2Cpages_show_list%2Cread_insights%2Cread_audience_network_insights%2Cpages_manage_cta";
+        "https://www.facebook.com/v4.0/dialog/oauth?response_type=token&client_id=2072731952831318&redirect_uri=https://socialanalytics-94264.web.app/addaccountfacebook&auth_type=rerequest&scope=public_profile%2Cmanage_pages%2Cpages_messaging%2Cpages_show_list%2Cread_insights%2Cread_audience_network_insights%2Cpages_manage_cta";
   }
   setGoalFacebook() {
     let user = firebase.auth().currentUser.uid;
--- src/components/dashboard/topbar.js
+++ src/components/dashboard/topbar.js
@@ -189,7 +189,7 @@ class Topbar extends React.Component {
                 className="btn instagram"
                 onClick={() =>
                   (window.location.href =
-                    "https://api.instagram.com/oauth/authorize/?client_id=c3ef63a1c9de41a1b6032a7c39e586ae&redirect_uri=http://localhost:3000/addaccountinstagram&response_type=token&instagram&scope=public_content")
+                    "https://api.instagram.com/oauth/authorize/?client_id=c3ef63a1c9de41a1b6032a7c39e586ae&redirect_uri=https://socialanalytics-94264.web.app/addaccountinstagram&response_type=token&instagram&scope=public_content")
                 }
               >
                 Instagram
@@ -198,7 +198,7 @@ class Topbar extends React.Component {
                 className="btn facebook"
                 onClick={() =>
                   (window.location.href =
-                    "https://www.facebook.com/v4.0/dialog/oauth?response_type=token&client_id=2072731952831318&redirect_uri=http://localhost:3000/addaccountfacebook&auth_type=rerequest&scope=public_profile%2Cmanage_pages%2Cpublish_pages%2Cpages_messaging%2Cpages_show_list%2Cread_insights%2Cread_audience_network_insights%2Cpages_manage_cta")
+                    "https://www.facebook.com/v4.0/dialog/oauth?response_type=token&client_id=2072731952831318&redirect_uri=https://socialanalytics-94264.web.app/addaccountfacebook&auth_type=rerequest&scope=public_profile%2Cmanage_pages%2Cpublish_pages%2Cpages_messaging%2Cpages_show_list%2Cread_insights%2Cread_audience_network_insights%2Cpages_manage_cta")
                 }
               >
                 Facebook
